8281d64af6710911a00730ff5eab6bfbf647a944
kosaj <kosaj@users.noreply.github.com>
2021-12-04 13:31:34 +0100

    dockerized with reverse proxy

--- example-client/src/app/app.component.ts
+++ example-client/src/app/app.component.ts
@@ -1,10 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'example-client';
+
+  constructor(private readonly httpClient: HttpClient) {}
+
+  send(port?: string): void {
+    this.httpClient
+      .get(`${window.origin}${port}/test`)
+      .subscribe((response) => console.log(response));
+  }
 }
